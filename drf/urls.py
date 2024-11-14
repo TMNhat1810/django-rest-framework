681cebf6f767ef5a3c33c56df7f18b7d705eaaba
TMNhat1810 <minhnhat912002@gmail.com>
2024-11-14 11:03:08 +0700

    tutorial 4: authen and author

--- drf/urls.py
+++ drf/urls.py
@@ -1,11 +1,4 @@
 from django.urls import include, path
-from rest_framework import routers
-
-from tutorial.quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
